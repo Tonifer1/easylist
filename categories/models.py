47f7d85fcb93477dad18333456216afe07a70b76
Toni Pekkala <toni.pekkala@hotmail.com>
2025-10-01 21:04:54 +0300

    Lisätty max_length Charfield 40 categorynameen

--- categories/models.py
+++ categories/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
-    category_name = models.CharField(max_length=20)
+    category_name = models.CharField(max_length=40)
     image_link = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
